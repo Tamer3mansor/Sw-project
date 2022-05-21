9d9b922958d5a717e674fe741eb1d83c4a78e90a
Shrouk Yasser <101811358+SHYA95@users.noreply.github.com>
2022-05-21 23:08:39 +0200

    Revert "Merge pull request #3 from Tamer3mansor/csb-hls3m3"
    This reverts commit fd1f0015dd966e27d71171ad8e50f73d777e7604, reversing
    changes made to 50ee1061b6b8b7b7e13cc9b36f26ecd03981f563.

--- src/home/home.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Home from "./home";
-
-describe("Home Component Rendered", () => {
-  let wrapper = shallow(<Home />);
-
-  test(wrapper.find("div"));
-});
